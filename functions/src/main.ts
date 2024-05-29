91c3eff9b7424d5f406c03d787ca3f8e25902248
Anton Kuchmasov <anton.kuchmasov.dev@gmail.com>
2024-05-29 15:00:11 +0300

    enabled CORS for backend

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -3,8 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  });
   await app.listen(3000);
 }
 bootstrap();
